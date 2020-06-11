049249b194fa88359e0d96389cc90b83f152b210
Olivier Breuleux <breuleux@gmail.com>
2020-06-11 19:02:05 -0400

    Pass arguments to download_dataset.sh

--- scripts/download.sh
+++ scripts/download.sh
@@ -2,10 +2,10 @@
 
 set -ex
 
-milarun dataset milarun.datasets:mnist
-milarun dataset milarun.datasets:fake_imagenet
-milarun dataset milarun.datasets:bsds500_reso
-milarun dataset milarun.datasets:wmt16
-milarun dataset milarun.datasets:wiki2
-milarun dataset milarun.datasets:coco
-milarun dataset milarun.datasets:ml20m
+milarun dataset milarun.datasets:mnist $@
+milarun dataset milarun.datasets:fake_imagenet $@
+milarun dataset milarun.datasets:bsds500_reso $@
+milarun dataset milarun.datasets:wmt16 $@
+milarun dataset milarun.datasets:wiki2 $@
+milarun dataset milarun.datasets:coco $@
+milarun dataset milarun.datasets:ml20m $@
