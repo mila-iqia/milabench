e38d276c6976e4a92d806cc519f5889cd6e6f75d
Olivier Breuleux <breuleux@gmail.com>
2022-03-21 16:12:00 -0400

    Add rates instrument

--- milabench/instruments.py
+++ milabench/instruments.py
@@ -17,6 +17,44 @@ def stop(ov):
         ov.given.where("step").skip(stop) >> ov.stop
 
 
+@gated("--rates")
+def rates(ov):
+    yield ov.phases.load_script
+
+    sync = None
+
+    def setsync(use_cuda):
+        if use_cuda:
+            nonlocal sync
+            import torch
+
+            sync = torch.cuda.synchronize
+
+    ov.given["?use_cuda"].first_or_default(False) >> setsync
+
+    times = (
+        ov.given.keep("step", "batch_size")
+        .augment(time=lambda: time.time_ns())
+        .pairwise()
+        .buffer_with_time(1.0)
+    )
+
+    @times.subscribe
+    def _(elems):
+        t = 0
+        if sync is not None:
+            t0 = time.time_ns()
+            sync()
+            t1 = time.time_ns()
+            t += t1 - t0
+
+        t += sum(e2["time"] - e1["time"] for e1, e2 in elems)
+        n = sum(e1["batch_size"] for e1, e2 in elems)
+        t /= 1_000_000_000
+
+        ov.give(rate=n / t)
+
+
 class GPUMonitor(Thread):
     def __init__(self, ov, delay):
         super().__init__(daemon=True)
