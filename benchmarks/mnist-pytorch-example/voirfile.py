0525d52203f0d9fbec3ce27591539b48eab08346
Olivier Breuleux <breuleux@gmail.com>
2022-04-04 04:34:55 -0400

    Improve mnist-pytorch-example voirfile

--- benchmarks/mnist-pytorch-example/voirfile.py
+++ benchmarks/mnist-pytorch-example/voirfile.py
@@ -1,5 +1,11 @@
+from milabench.opt import instrument_argparse
+
+
 def instrument_probes(ov):
     yield ov.phases.load_script
     ov.probe("//train > loss").kmap(loss=lambda loss: float(loss)).give()
     ov.probe("//train(data as batch) > #endloop_data as step").give()
     ov.probe("//main > train_loader as loader").first().give()
+    ov.probe(
+        "//train(data as batch, !#loop_data as compute_start, !!#endloop_data as compute_end)"
+    ).give()
