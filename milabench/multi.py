d78082ab469729a12453bab20eb84a570cf2c58b
Olivier Breuleux <breuleux@gmail.com>
2022-04-07 16:56:01 -0400

    Kill processes on exception, fixes #15

--- milabench/multi.py
+++ milabench/multi.py
@@ -1,5 +1,3 @@
-import json
-import os
 import subprocess
 import time
 from copy import deepcopy
@@ -53,38 +51,6 @@ def njobs(cfg, n):
             "job-number": i,
         }
         yield clone_with(cfg, gcfg)
-
-
-def multiread(pipes):
-    for (_, __, p) in pipes.values():
-        os.set_blocking(p.stdout.fileno(), False)
-
-    while pipes:
-        for tag, (pack, run, proc) in list(pipes.items()):
-
-            def _give(data):
-                data["#pack"] = pack
-                data["#run"] = run
-                give(**data)
-
-            while True:
-                line = proc.stdout.readline()
-                if not line:
-                    ret = proc.poll()
-                    if ret is not None:
-                        # We do not read a line and the process is over
-                        del pipes[tag]
-                        _give({"#end": time.time(), "#return_code": ret})
-                    break
-                line = line.decode("utf8")
-                try:
-                    data = json.loads(line)
-                    if not isinstance(data, dict):
-                        data = {"#stdout": data}
-                except json.JSONDecodeError:
-                    data = {"#stdout": line}
-                _give(data)
-        time.sleep(0.1)
 
 
 @ovld
@@ -145,8 +111,22 @@ class MultiPackage:
                     process = pack.run(args=args, voirargs=voirargs, env=env)
                     mr.add_process(process, info={"#pack": pack, "#run": run})
 
-                for _ in mr:
-                    time.sleep(0.1)
+                try:
+                    for _ in mr:
+                        time.sleep(0.1)
+
+                except BaseException as exc:
+                    for (proc, info) in mr.processes:
+                        errstring = f"{type(exc).__name__}: {exc}"
+                        endinfo = {
+                            "#end": time.time(),
+                            "#completed": False,
+                            "#error": errstring,
+                        }
+                        give(**endinfo, **info)
+                        proc.kill()
+                    if not isinstance(exc, KeyboardInterrupt):
+                        raise
 
     def do_dev(self, dash):
         # TODO: share the common code between do_run and do_dev
