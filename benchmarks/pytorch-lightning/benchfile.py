0b29b77368f5fa39b903e4b6b15fdd84a45e2c00
lebrice <fabrice.normandin@gmail.com>
2022-04-07 16:54:25 -0400

    Simplify benchfile, add frozen requirements
    Signed-off-by: lebrice <fabrice.normandin@gmail.com>

--- benchmarks/pytorch-lightning/benchfile.py
+++ benchmarks/pytorch-lightning/benchfile.py
@@ -1,11 +1,9 @@
-from __future__ import annotations
-
 from milabench.pack import Package
 
 
 class PLBenchmark(Package):
     # Requirements file installed by install(). It can be empty or absent.
-    requirements_file = "requirements.txt"
+    requirements_file = "frozen_reqs.txt"
 
     # The preparation script called by prepare(). It must be executable,
     # but it can be any type of script. It can be empty or absent.
@@ -14,27 +12,6 @@ class PLBenchmark(Package):
     # The main script called by run(). It must be a Python file. It has to
     # be present.
     main_script = "main.py"
-
-    # You can remove the functions below if you don't need to modify them.
-
-    def make_env(self):
-        # Return a dict of environment variables for prepare_script and
-        # main_script.
-        return super().make_env()
-
-    def install(self):
-        super().install()  # super() call installs the requirements
-
-    def prepare(self):
-        super().prepare()  # super() call executes prepare_script
-
-    def run(self, args: list[str], voirargs: list[str], env: dict):
-        # You can insert new arguments to args/voirargs or change the env,
-        # although changing the env is a bit simpler if you modify make_env
-        # TODO: Need to figure out how to setup multiple benchmarks here.
-        return super().run(args, voirargs, env)
-        # Note: run() must return a running process, so make sure not to lose
-        # the return value of super() here.
 
 
 __pack__ = PLBenchmark
