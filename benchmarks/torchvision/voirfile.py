3335d1ed9b0d6f58346455a753fd5798451c83f8
notoraptor <stevenbocco@gmail.com>
2022-04-08 16:13:26 -0400

    Use ptera selector to wrap model execution into a profiler function

--- benchmarks/torchvision/voirfile.py
+++ benchmarks/torchvision/voirfile.py
@@ -5,70 +5,57 @@ import torch.profiler as profiler
 import torch
 
 
-class ModelCallWrapper:
+def _profile_with_deepspeed(**kwargs):
     """
-    Wrapper to replace model.__call__ method with a wrapper
-    which must profile each call.
+    Use deepspeed profiler to profile model() call.
+    Fast, but raises a RecursionError
+    ("maximum recursion depth exceeded while calling a Python object")
+    for big data sizes (--data-size >= 976)
     """
+    # Reference:
+    # https://www.deepspeed.ai/tutorials/flops-profiler/#example-training-workflow
+    model = kwargs["model"]
+    prof = FlopsProfiler(model)
 
-    def __init__(self, model):
-        cls = type(model)
-        self.model = model
-        self.original_call = cls.__call__
-        self.measures = []
-        cls.__call__ = self.call_torch_profiler
+    prof.start_profile()
+    yield
+    prof.stop_profile()
 
-    def call_deepspeed(self, *args, **kwargs):
-        """
-        Use deepspeed profiler to profile model() call.
-        Fast, but raises a RecursionError
-        ("maximum recursion depth exceeded while calling a Python object")
-        for big data sizes (--data-size >= 976)
-        """
-        # Reference:
-        # https://www.deepspeed.ai/tutorials/flops-profiler/#example-training-workflow
-        prof = FlopsProfiler(self.model)
+    flops = prof.get_total_flops()
+    macs = prof.get_total_macs()
+    params = prof.get_total_params()
+    prof.end_profile()
+    print(flops, macs, params)
 
-        prof.start_profile()
-        ret = self.original_call(self.model, *args, **kwargs)
-        prof.stop_profile()
+    return flops, macs, params
 
-        flops = prof.get_total_flops()
-        macs = prof.get_total_macs()
-        params = prof.get_total_params()
-        self.measures.append((flops, macs, params))
-        prof.end_profile()
-        print(len(self.measures), flops, macs, params)
 
-        return ret
-
-    def call_torch_profiler(self, *args, **kwargs):
-        """
-        Use torch.profiler to profile model() call.
-        Slow, but no bugs.
-        """
-        with profiler.profile(
-            activities=[
-                profiler.ProfilerActivity.CPU,
-                profiler.ProfilerActivity.CUDA,
-            ],
-            # schedule=profiler.schedule(wait=0, warmup=0, active=1),
-            record_shapes=True,
-            profile_memory=True,
-            with_stack=True,
-            with_flops=True,
-            with_modules=True,
-        ) as prof:
-            ret = self.original_call(self.model, *args, **kwargs)
-            torch.cuda.synchronize()
-        evts = prof.key_averages()
-        # evts is a torch event list:
-        # https://github.com/pytorch/pytorch/blob/9905b1f29a5d23d30cfb570679845766983260c4/torch/autograd/profiler_util.py#L14
-        # Each event is an instance of torch.autograd.profiler_util.FunctionEventAvg:
-        # https://github.com/pytorch/pytorch/blob/9905b1f29a5d23d30cfb570679845766983260c4/torch/autograd/profiler_util.py#L521
-        self.measures.append(evts)
-        print(len(self.measures), sum(evt.flops for evt in evts), len(evts), type(evts), type(evts[0]))
-        return ret
+def _profile_with_torch_profiler():
+    """
+    Use torch.profiler to profile model() call.
+    Slow, but no bugs.
+    """
+    with profiler.profile(
+        activities=[
+            profiler.ProfilerActivity.CPU,
+            profiler.ProfilerActivity.CUDA,
+        ],
+        # schedule=profiler.schedule(wait=0, warmup=0, active=1),
+        record_shapes=True,
+        profile_memory=True,
+        with_stack=True,
+        with_flops=True,
+        with_modules=True,
+    ) as prof:
+        yield
+        torch.cuda.synchronize()
+    evts = prof.key_averages()
+    # evts is a torch event list:
+    # https://github.com/pytorch/pytorch/blob/9905b1f29a5d23d30cfb570679845766983260c4/torch/autograd/profiler_util.py#L14
+    # Each event is an instance of torch.autograd.profiler_util.FunctionEventAvg:
+    # https://github.com/pytorch/pytorch/blob/9905b1f29a5d23d30cfb570679845766983260c4/torch/autograd/profiler_util.py#L521
+    print(sum(evt.flops for evt in evts), len(evts), type(evts), type(evts[0]))
+    return evts
 
 
 def instrument_probes(ov):
@@ -79,19 +66,13 @@ def instrument_probes(ov):
     ov.probe("//train_epoch > loader").give()
     ov.probe("//train_epoch(inp as batch) > #endloop_inp as step").give()
     ov.probe(
-        "//train_epoch(inp as batch, !#loop_inp as compute_start, !!#endloop_inp as compute_end)"
+        "//train_epoch(inp as batch, model, !#loop_inp as compute_start, !!#endloop_inp as compute_end)"
     ).give()
 
-    def check(model):
-        wrapper = ModelCallWrapper(model)
-        ov.give(wrapper=wrapper)
-
-    given_model = ov.given["?model"]
-    given_model.subscribe(check)
+    ov.given.wmap("compute_start", _profile_with_deepspeed).give("profiler_stats")
 
 
 def instrument_print_profiles(ov):
-    wrapper_cell = ov.given["?wrapper"].accum()
+    cells = ov.given["?profiler_stats"].accum()
     yield ov.phases.run_script
-    wrapper: ModelCallWrapper = wrapper_cell[0]
-    print("Wrapper nb. calls", len(wrapper.measures))
+    print(len(cells), type(cells[0]))
