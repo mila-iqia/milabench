6f78f6389f163b4ac427530d1d380531fd3ae795
Bruno Carrez <bruno@carrez.net>
2022-04-14 17:32:33 -0400

    Add support for building singularity/apptainer containers  (#18)

--- milabench/cli.py
+++ milabench/cli.py
@@ -293,16 +293,23 @@ class Main:
 
         # Optional path to copy build dir to, instead of building the image.
         # This directory must not exist and will be created.
-        # [alias: -o]
         output_dir: Option & str = None
+
+        # File in which to generate the SIF image (Singularity).
+        # Defaults to milabench.sif.
+        # [alias: -o]
+        output_file: Option & str = None
 
         # Optional python version to use for the image, ignored for
         # conda-based benchmarks. Can be specified as any of
         # ('3', '3.9', '3.9.2')
         python_version: Option & str = "3.9"
 
+        # Milabench source to clone from
+        milabench: Option & str = "v2"
+
         # The tag for the generated container
-        tag: Option & str = "milabench"
+        tag: Option & str = None
 
         if type not in ["docker", "singularity"]:
             sys.exit(f"Unsupported type {type}")
@@ -333,11 +340,19 @@ class Main:
                 defn["venv"]["type"] == "conda" for defn in benchmarks.values()
             )
 
+            if "//" not in milabench:
+                milabench = (
+                    f"git+https://github.com/mila-iqia/milabench.git@{milabench}"
+                )
+
             if type == "docker":
+                if output_file is not None:
+                    sys.exit("Error: --output-file only valid with Singularity")
+                tag = tag or "milabench"
                 with (root / "Dockerfile").open("w") as f:
                     f.write(
                         dockerfile_template(
-                            milabench_req="git+https://github.com/mila-iqia/milabench.git@v2",
+                            milabench_req=milabench,
                             include_data=include_data,
                             use_conda=use_conda,
                             python_version=python_version,
@@ -348,13 +363,38 @@ class Main:
                     root.copy(output_dir)
                 else:
                     subprocess.check_call(["docker", "build", ".", "-t", tag], cwd=root)
+
             elif type == "singularity":
-                raise NotImplementedError(type)
+                if tag is not None:
+                    sys.exit("Error: --tag only valid with Docker")
+                output_file = output_file or "milabench.sif"
+
+                with (root / "milabench.def").open("w") as f:
+                    f.write(
+                        singularitydef_template(
+                            milabench_req=milabench,
+                            include_data=include_data,
+                            use_conda=use_conda,
+                            python_version=python_version,
+                            config_file=config_file.relative_to(common_base),
+                        )
+                    )
+                if output_dir:
+                    root.copy(output_dir)
+                else:
+                    user = os.environ["USER"]
+                    filename = str(XPath(output_file).absolute())
+                    subprocess.check_call(
+                        ["sudo", "singularity", "build", filename, "milabench.def"],
+                        cwd=root,
+                    )
+                    subprocess.check_call(["sudo", "chown", f"{user}:{user}", filename])
 
 
 def dockerfile_template(
     milabench_req, include_data, use_conda, python_version, config_file
 ):
+    conda_clean = "conda clean -a" if use_conda else "echo"
     return f"""
 FROM { 'continuumio/miniconda3' if use_conda else f'python:{python_version}-slim' }
 
@@ -362,12 +402,12 @@ RUN apt-get update && apt-get install --no-install-suggests --no-install-recomme
     git \
     wget \
     patch \
- && rm -rf /var/lib/apt/lists/*
+ && apt-get clean
 
 RUN mkdir /bench && mkdir /base
 ENV MILABENCH_BASE /base
 # This is to signal to milabench to use that as fallback
-ENV VIRTUAL_ENV /base/venv
+ENV VIRTUAL_ENV /base/venv/_
 ENV MILABENCH_DEVREQS /version.txt
 ENV MILABENCH_CONFIG /bench/{ config_file }
 ENV HEADLESS 1
@@ -380,9 +420,54 @@ COPY / /bench
 RUN pip install -U pip && \
     pip install -r /version.txt && \
     milabench install && \
-    rm -rf /root/.cache/pip
+    { conda_clean } && \
+    pip cache purge
 
 { 'RUN milabench prepare' if include_data else '' }
 
 CMD ["milabench", "run"]
+"""
+
+
+def singularitydef_template(
+    milabench_req, include_data, use_conda, python_version, config_file
+):
+    conda_clean = "conda clean -a" if use_conda else "echo"
+    return f"""\
+BootStrap: docker
+From: { 'continuumio/miniconda3' if use_conda else f'python:{python_version}-slim' }
+
+%files
+    . /bench
+
+%environment
+    export MILABENCH_BASE=/base
+    export MILABENCH_DEVREQS=/version.txt
+    export MILABENCH_CONFIG=/bench/{ config_file }
+    export HEADLESS=1
+
+%post
+    export MILABENCH_BASE=/base
+    export MILABENCH_DEVREQS=/version.txt
+    export MILABENCH_CONFIG=/bench/{ config_file }
+    export HEADLESS=1
+
+    apt-get update && apt-get install --no-install-suggests --no-install-recommends -y git wget patch
+    apt-get clean
+
+    mkdir /base
+    cd /bench
+
+    echo '{ milabench_req }' > /version.txt
+    pip install -U pip && \
+    pip install -r /version.txt && \
+    milabench install && \
+    { conda_clean } && \
+    pip cache purge
+{ '    milabench prepare' if include_data else '' }
+
+    chmod -R o+rwx /base /bench
+
+%runscript
+    milabench run
 """
