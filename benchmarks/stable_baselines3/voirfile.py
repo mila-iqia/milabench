c11a399a53be40a0d464d2731c41534ede3e35b8
soline-b <85968577+soline-b@users.noreply.github.com>
2022-04-19 14:39:59 -0400

    Support TD3 from stable_baselines3 (#19)

--- benchmarks/stable_baselines3/voirfile.py
+++ benchmarks/stable_baselines3/voirfile.py
@@ -2,6 +2,7 @@ import torch
 
 # need to import it because it is not yet loaded once the main script is loaded
 from stable_baselines3.ppo.ppo import PPO
+from stable_baselines3.td3.td3 import TD3
 
 
 def instrument_probes(ov):
@@ -9,6 +10,8 @@ def instrument_probes(ov):
     # https://breuleux.github.io/milabench/instrument.html#probing-for-milabench
 
     yield ov.phases.load_script
+
+    # PPO
 
     # Get device
     ov.probe("/stable_baselines3.ppo.ppo/PPO/train > self").kmap(
@@ -31,4 +34,24 @@ def instrument_probes(ov):
     # Compute Start & End + Batch
     ov.probe(
         "/stable_baselines3.ppo.ppo/PPO/train(rollout_data as batch, !#loop_rollout_data as compute_start, !!#endloop_rollout_data as compute_end)"
+    ).give()
+
+    # TD3
+
+    # Loss
+    (
+        ov.probe("/stable_baselines3.td3.td3/TD3/train > actor_loss")
+        .throttle(1)["actor_loss"]
+        .map(float)
+        .give("loss")
+    )
+
+    # Compute step
+    ov.probe(
+        "/stable_baselines3.td3.td3/TD3/train(_ as batch) > #endloop__ as step"
+    ).give()
+
+    # Compute Start & End + Batch
+    ov.probe(
+        "/stable_baselines3.td3.td3/TD3/train(_ as batch, !#loop__ as compute_start, !!#endloop__ as compute_end)"
     ).give()
