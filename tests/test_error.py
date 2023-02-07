fd703fb2d64c32eba7faaae754925adcde374dc8
Pierre Delaunay <pierre@delaunay.io>
2023-02-07 11:01:23 -0500

    Change how we check the output

--- tests/test_error.py
+++ tests/test_error.py
@@ -4,51 +4,21 @@ import pytest
 from milabench.testing import milabench_cmd, config
 
 
-expected_out_short = """
-Error Report
-------------
-(.*)
-hf_t5.D0
-^^^^^^^^
-    ValueError: invalid literal for int() with base 10: 'xyz'
-(.*)
-Summary
--------
-    Success: 0
-    Failures: 1
-""".strip()
-
-expected_out_long = """
-Error Report
-------------
-(.*)
-hf_t5.D0
-^^^^^^^^
-    Traceback (most recent call last):
-    File "(.*)", line 2476, in _get_value
-        result = type_func(arg_string)
-    ValueError: invalid literal for int() with base 10: 'xyz'
-(.*)
-Summary
--------
-    Success: 0
-    Failures: 1
-""".strip()
-
-error_cases = [([], expected_out_short), (["--fulltrace"], expected_out_long)]
+def short_matcher(output):
+    """Check that the last line of the traceback is found"""
+    return "ValueError: invalid literal for int() with base 10: 'xyz'" in output
 
 
-def make_regex(pattern):
-    frags = []
-
-    for pat in pattern.strip().split("(.*)"):
-        frags.append(re.escape(pat))
-
-    return "(.*)" + "(.*)".join(frags) + "(.*)"
+def long_matcher(output):
+    """Check that at least another line of the traceback is printed as well"""
+    return short_matcher(output) and "result = type_func(arg_string)" in output
 
 
-@pytest.mark.parametrize("args,expected_out", error_cases)
-def test_error_reporting_short(capsys, args, expected_out):
+error_cases = [([], short_matcher), (["--fulltrace"], long_matcher)]
+
+
+@pytest.mark.parametrize("args,matcher", error_cases)
+def test_error_reporting_short(capsys, args, matcher):
 
     with pytest.raises(SystemExit) as err:
         milabench_cmd("run", config("argerror"), *args)
@@ -56,11 +26,6 @@ def test_error_reporting_short(capsys, args, expected_out):
     assert err.type is SystemExit
     assert err.value.code == -1
 
-    expected_pat = re.compile(
-        make_regex(expected_out),
-        flags=re.MULTILINE | re.DOTALL,
-    )
-
     captured = capsys.readouterr()
-    assert re.search(expected_pat, captured.out)
+    assert matcher(captured.out), "The traceback need to be printed"
     assert captured.err == ""
