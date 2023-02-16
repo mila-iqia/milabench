8598a4626fec708e9f75473f9b91885dcdf98d0e
Setepenre <pierre.delaunay.tr@gmail.com>
2023-02-16 13:20:10 -0500

    Update push_image.bash

--- script/push_image.bash
+++ script/push_image.bash
@@ -1,18 +1,24 @@
 #!/bin/bash
 
+# Show how to manually build & Push a docker image
+
+ARCH=${ARCH:-rocm}
+TAG=${TAG:-nightly} 
+USER=${USER:-} 
+TOKEN =${TOKEN:-} 
 
 build_docker {
     # Build docker
-    sudo docker build                           \
-        -t milabench:nightly-rocm               \
-        --build-arg ARCH=rocm                   \
-        --build-arg CONFIG=standard-rocm.yaml   \
+    sudo docker build                            \
+        -t milabench:${ARCH}-${TAG}              \
+        --build-arg ARCH=${ARCH}                 \
+        --build-arg CONFIG=standard-${ARCH}.yaml \
         .
 }
 
 push_docker {
     # Push the image to github
     echo $TOKEN | docker login ghcr.io -u $USER --password-stdin 
-    docker image tag milabench:nightly-rocm ghcr.io/$USER/milabench:nightly-rocm
-    docker push ghcr.io/$USER/milabench:nightly-cuda
+    docker image tag milabench:${ARCH}-${TAG} ghcr.io/$USER/milabench:${ARCH}-${TAG}
+    docker push ghcr.io/$USER/milabench:${ARCH}-${TAG}
 }
