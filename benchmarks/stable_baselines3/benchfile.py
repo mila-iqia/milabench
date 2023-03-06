06081a1fea83470a778da613820a2514834b3af9
satyaog <satya.ortiz-gagne@mila.quebec>
2023-03-06 14:55:42 -0500

    Standardize dependency management (#49)

--- benchmarks/stable_baselines3/benchfile.py
+++ benchmarks/stable_baselines3/benchfile.py
@@ -4,23 +4,18 @@ BRANCH = "168c34e8f5c19a34c1b372004e3a840bd400a9f6"
 
 
 class StableBenchmarkPack(Package):
-    # Requirements file installed by install(). It can be empty or absent.
-    requirements_file = "requirements.txt"
+    requirements_file = "requirements-bench.txt"
 
     def install(self):
         """Install requirements one by one, as baselines setup is non-standard"""
+        code = self.dirs.code
+
+        # Install swig first as it is required to compile box2d-py
+        self.pip_install("-r", code / "requirements-pre.txt")
+
         super().install()
 
-        code = self.dirs.code
         code.clone_subtree("https://github.com/DLR-RM/rl-baselines3-zoo", BRANCH)
-
-        reqfile = code / "requirements.txt"
-        reqfile.sub("box2d-py", "# box2d-py")
-
-        # this dependency requires swig, use conda and forget about it
-        self.conda_install("-c", "conda-forge", "pybox2d")
-
-        self.pip_install("-r", reqfile)
 
     def run(self, args, voirargs, env):
         return self.launch("train.py", args=args, voirargs=voirargs, env=env)
