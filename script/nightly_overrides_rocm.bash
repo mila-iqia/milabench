7657ca25a92d11418e22d0c90582593d0db27792
Setepenre <pierre.delaunay.tr@gmail.com>
2023-03-24 11:27:03 -0400

    Use the latest pytorch 2.0.0

--- script/nightly_overrides_rocm.bash
+++ script/nightly_overrides_rocm.bash
@@ -1,5 +1,5 @@
 milabench pip uninstall torch torchvision torchaudio torchtext -y
-milabench pip install --pre torch torchvision torchaudio torchtext --index-url https://download.pytorch.org/whl/nightly/rocm5.4.2
+milabench pipinstall torch==2.0.0 torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm5.4.2
 milabench pip install -U pytorch-lightning
 milabench pip uninstall lightning-bolts==1.9.4 -y
 milabench pip install git+https://github.com/Delaunay/lightning-bolts.git@patch-1
