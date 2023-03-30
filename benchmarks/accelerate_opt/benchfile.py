6bbd5314b0a6918f1fefc36b339ce3b8de6e1b95
Arnaud Bergeron <abergeron@gmail.com>
2023-03-30 16:54:14 -0400

    multiple fixes to the multi-node run

--- benchmarks/accelerate_opt/benchfile.py
+++ benchmarks/accelerate_opt/benchfile.py
@@ -24,9 +24,15 @@ class AccelerateBenchmark(Package):
             env={"MILABENCH_PREPARE_ONLY": "1"},
         )
 
-    async def run_remote_command(host, command):
+    async def run_remote_command(self, host, command):
         async with asyncssh.connect(host) as conn:
-            return await conn.run(command)
+            return await conn.run(command,
+                                  # disable host key validation
+                                  known_hosts=None,
+                                  # use our key to authenticate
+                                  client_keys=[asyncssh.read_private_key(self.dirs.code / "id_milabench")],
+                                  username="milabench",
+            )
 
     async def run(self):
         self.phase = "run"
@@ -50,7 +56,7 @@ class AccelerateBenchmark(Package):
             use_stdout=True,
         ))
         # XXX: this doesn't participate in the process timeout
-        for worker in self.config['workers']:
+        for worker in self.config['worker_addrs']:
             futs.append(self.run_remote_command(
                 worker,
                 f"docker run --rm --network host --gpus all "
@@ -58,8 +64,8 @@ class AccelerateBenchmark(Package):
                 # XXX: figure out the right path
                 f"milabench run /standard.yaml "
                 f"--select {self.config['name']} "
-                f"--override manager_addr={self.config['manager_addr']} "
-                f"--override manager_port={self.config['manager_port']}"
+                f"--override manager_addr='{repr(self.config['manager_addr'])}' "
+                f"--override manager_port='{repr(self.config['manager_port'])}'"
                 )
             )
         _, pending = await asyncio.wait(futs, return_when=asyncio.FIRST_EXCEPTION)
