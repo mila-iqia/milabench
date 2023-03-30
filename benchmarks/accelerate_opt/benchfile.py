e58de828deae6d2984784c6be3190816dbd2e58a
Arnaud Bergeron <abergeron@gmail.com>
2023-03-30 12:33:48 -0400

    Add preliminary support for multi-node

--- benchmarks/accelerate_opt/benchfile.py
+++ benchmarks/accelerate_opt/benchfile.py
@@ -1,4 +1,5 @@
 from milabench.pack import Package
+import asyncssh
 
 
 class AccelerateBenchmark(Package):
@@ -11,40 +12,57 @@ class AccelerateBenchmark(Package):
 
     async def prepare(self):
         self.phase = "prepare"
-        nproc = len(self.config.get("devices", []))
         await self.execute(
             "accelerate",
             "launch",
             "--mixed_precision=fp16",
             "--num_machines=1",
             "--dynamo_backend=no",
-            f"--num_processes={nproc}",
+            "--num_processes=1",
+            "--num_cpu_threads_per_process=8",
             str(self.dirs.code / "main.py"),
             env={"MILABENCH_PREPARE_ONLY": "1"},
         )
 
+    async def run_remote_command(host, command):
+        async with asyncssh.connect(host) as conn:
+            return await conn.run(command)
+
     async def run(self):
         self.phase = "run"
         nproc = len(self.config.get("devices", []))
-        await self.execute(
+        futs = []
+        futs.append(self.execute(
             "accelerate",
             "launch",
             "--mixed_precision=fp16",
             "--dynamo_backend=no",
-            "--num_machines=1",
+            f"--num_machines={self.config['num_machines']}",
             "--use_deepspeed",
-            "--deepspeed_multinode_launcher=standard",
             f"--gradient_accumulation_steps={self.config['gradient_accumulation_steps']}",
             "--zero_stage=2",
             f"--num_cpu_threads_per_process={self.config['cpus_per_gpu']}",
             f"--main_process_ip={self.config['manager_addr']}",
             f"--main_process_port={self.config['manager_port']}",
-            # f"--num_processes={self.config['num_processes']}",
             f"--num_processes={nproc}",
             str(self.dirs.code / "main.py"),
             setsid=True,
             use_stdout=True,
-        )
-
+        ))
+        # XXX: this doesn't participate in the process timeout
+        for worker in self.config['workers']:
+            futs.append(self.run_remote_command(
+                worker,
+                f"docker run --rm --network host --gpus all "
+                f"{self.config['docker_image']} "
+                # XXX: figure out the right path
+                f"milabench run /standard.yaml "
+                f"--select {self.config['name']} "
+                f"--override manager_addr={self.config['manager_addr']} "
+                f"--override manager_port={self.config['manager_port']}"
+                )
+            )
+        _, pending = await asyncio.wait(futs, return_when=asyncio.FIRST_EXCEPTION)
+        # XXX: what to do with the pendings?
 
 __pack__ = AccelerateBenchmark
