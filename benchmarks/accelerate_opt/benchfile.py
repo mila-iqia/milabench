903ee8da7cbd6063e6a9ddbb707008609e867127
Arnaud Bergeron <abergeron@gmail.com>
2023-03-31 16:35:41 -0400

    Even more tweaks to get the remote run working

--- benchmarks/accelerate_opt/benchfile.py
+++ benchmarks/accelerate_opt/benchfile.py
@@ -1,5 +1,6 @@
 from milabench.pack import Package
 import asyncssh
+import asyncio
 
 
 class AccelerateBenchmark(Package):
@@ -25,26 +26,31 @@ class AccelerateBenchmark(Package):
         )
 
     async def run_remote_command(self, host, command):
-        async with asyncssh.connect(host) as conn:
-            return await conn.run(command,
-                                  # disable host key validation
-                                  known_hosts=None,
-                                  # use our key to authenticate
-                                  client_keys=[asyncssh.read_private_key(self.dirs.code / "id_milabench")],
-                                  username="milabench",
-            )
+        print(f"Connecting to worker: {host}")
+        async with asyncssh.connect(host,
+                                    # disable host key validation
+                                    known_hosts=None,
+                                    # use our key to authenticate
+                                    client_keys=[asyncssh.read_private_key(self.dirs.code / "id_milabench")],
+                                    username="milabench",
+        ) as conn:
+            print(f"Running command: {command}")
+            path = str(self.dirs.runs / self.config['run_name'] / host)
+            return await conn.run(command, stderr=path + ".stderr",
+                                  stdout=path + ".stdout")
 
-    async def run(self):
-        self.phase = "run"
+
+    def accelerate_command(self, rank):
         nproc = len(self.config.get("devices", []))
-        futs = []
-        futs.append(self.execute(
+        return [
             "accelerate",
             "launch",
             "--mixed_precision=fp16",
             "--dynamo_backend=no",
+            f"--machine_rank={rank}",
             f"--num_machines={self.config['num_machines']}",
             "--use_deepspeed",
+            "--deepspeed_multinode_launcher=standard",
             f"--gradient_accumulation_steps={self.config['gradient_accumulation_steps']}",
             "--zero_stage=2",
             f"--num_cpu_threads_per_process={self.config['cpus_per_gpu']}",
@@ -52,23 +58,30 @@ class AccelerateBenchmark(Package):
             f"--main_process_port={self.config['manager_port']}",
             f"--num_processes={nproc}",
             str(self.dirs.code / "main.py"),
+        ]
+
+    async def run(self):
+        self.phase = "run"
+        futs = []
+        futs.append(self.execute(
+            *self.accelerate_command(rank=0),
             setsid=True,
             use_stdout=True,
         ))
         # XXX: this doesn't participate in the process timeout
-        for worker in self.config['worker_addrs']:
-            futs.append(self.run_remote_command(
-                worker,
-                f"docker run --rm --network host --gpus all "
-                f"{self.config['docker_image']} "
-                # XXX: figure out the right path
-                f"milabench run /standard.yaml "
-                f"--select {self.config['name']} "
-                f"--override manager_addr='{repr(self.config['manager_addr'])}' "
-                f"--override manager_port='{repr(self.config['manager_port'])}'"
-                )
-            )
-        _, pending = await asyncio.wait(futs, return_when=asyncio.FIRST_EXCEPTION)
-        # XXX: what to do with the pendings?
+        for i, worker in enumerate(self.config['worker_addrs']):
+            command = ["docker", "run", "-i", "--rm",
+                       "--network", "host",
+                       "--gpus", "all"]
+            env = self.make_env()
+            for var in ('MILABENCH_CONFIG', 'XDG_CACHE_HOME', 'OMP_NUM_THREADS'):
+                command.append("--env")
+                command.append(f"{var}='{env[var]}'")
+            command.append(f"{self.config['docker_image']}")
+            command.append(f"{self.dirs.code / 'activator'}")
+            command.append(f"{self.dirs.venv}")
+            command.extend(self.accelerate_command(rank=i + 1))
+            futs.append(self.run_remote_command(worker, " ".join(command)))
+        await asyncio.gather(*futs)
 
 __pack__ = AccelerateBenchmark
