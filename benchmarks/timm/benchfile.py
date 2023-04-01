b1f80ec6f19c8a411c6b8caae43a0c82355c25f6
Olivier Breuleux <breuleux@gmail.com>
2023-03-31 23:52:21 -0400

    Enable multi-gpu for timm benchmarks

--- benchmarks/timm/benchfile.py
+++ benchmarks/timm/benchfile.py
@@ -7,15 +7,21 @@ class TimmBenchmarkPack(Package):
     base_requirements = "requirements.in"
     main_script = "pytorch-image-models/train.py"
 
+    def make_env(self):
+        return {
+            **super().make_env(),
+            "OMP_NUM_THREADS": str(self.config.get("cpus_per_gpu", 8))
+        }
+
     @property
     def argv(self):
-        extra_args = [
+        return [
+            *super().argv,
             "--data-dir", self.dirs.data,
             "--dataset", "FakeImageNet",
             "--output", self.dirs.extra / self.logdir.name / self.tag,
             "--checkpoint-hist", 1,
         ]
-        return [*super().argv, *extra_args]
 
     async def install(self):
         await super().install()
@@ -23,6 +29,21 @@ class TimmBenchmarkPack(Package):
         timm = self.dirs.code / "pytorch-image-models"
         if not timm.exists():
             timm.clone_subtree("https://github.com/huggingface/pytorch-image-models", BRANCH)
+
+    async def run(self):
+        main = self.dirs.code / self.main_script
+        assert main.exists()
+        nproc = self.config.get("nproc", 1)
+        if nproc > 1:
+            return await self.voir(
+                self.main_script,
+                args=self.argv,
+                cwd=main.parent,
+                wrapper=["torchrun", f"--nproc_per_node={nproc}", "-m"],
+                use_stdout=True,
+            )
+        else:
+            return await self.voir(self.main_script, args=self.argv, cwd=main.parent)
 
 
 __pack__ = TimmBenchmarkPack
--- benchmarks/timm/voirfile.py
+++ benchmarks/timm/voirfile.py
@@ -26,36 +26,45 @@ class Config:
 
 @configurable
 def instrument_main(ov, options: Config):
-    import torch
+    def setup(args):
+        import torch
 
-    yield ov.phases.init
+        if options.dash:
+            ov.require(dash)
 
-    if options.dash:
-        ov.require(dash)
+        ov.require(
+            log("value", "progress", "rate", "units", "loss", "gpudata", context="task"),
+            rate(
+                interval=options.interval,
+                skip=options.skip,
+                sync=torch.cuda.synchronize if torch.cuda.is_available() else None,
+                batch_size_calc=lambda b: len(b) * args.world_size
+            ),
+            early_stop(n=options.stop, key="rate", task="train", signal="stop"),
+            gpu_monitor(poll_interval=options.gpu_poll),
+        )
 
-    ov.require(
-        log("value", "progress", "rate", "units", "loss", "gpudata", context="task"),
-        rate(
-            interval=options.interval,
-            skip=options.skip,
-            sync=torch.cuda.synchronize if torch.cuda.is_available() else None,
-        ),
-        early_stop(n=options.stop, key="rate", task="train"),
-        gpu_monitor(poll_interval=options.gpu_poll),
-    )
+        # Loss
+        (
+            loss_probe
+            .throttle(1)["loss"]
+            .map(lambda loss: {"task": "train", "loss": float(loss)})
+            .give()
+        )
+
+        # Compute Start & End + Batch
+        batch_probe.augment(task=lambda: "train").give()
 
     yield ov.phases.load_script
 
+    from timm.utils.distributed import is_global_primary
+
     # Loss
-    (
-        ov.probe("//train_one_epoch > loss")
-        .throttle(1)["loss"]
-        .map(lambda loss: {"task": "train", "loss": float(loss)})
-        .give()
-    )
+    loss_probe = ov.probe("//train_one_epoch > loss")
 
     # Compute Start & End + Batch
-    ov.probe(
+    batch_probe = ov.probe(
         "//train_one_epoch(input as batch, !#loop_batch_idx as step, !!#endloop_batch_idx as step_end)"
-    ).augment(task=lambda: "train").give()
+    )
 
+    ov.probe("//main(args) > device")["args"].filter(is_global_primary).subscribe(setup)
