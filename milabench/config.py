b7e58d06674bd31ae987686065e93d7f8a4e4737
Pierre Delaunay <pierre@delaunay.io>
2023-04-03 12:10:59 -0400

    split bench meta and definition

--- milabench/config.py
+++ milabench/config.py
@@ -4,11 +4,27 @@ from .fs import XPath
 from .merge import self_merge
 
 
+def load_meta(config_base, metafile):
+    with open(config_base / metafile) as cf:
+        config = yaml.safe_load(cf)
+        
+    return config
+
+
+def insert_meta(meta, defn):
+    name = defn["name"]
+    metadata = meta.get(name, dict())
+    for k, v in metadata.items():
+        if k not in defn:
+            defn[k] = v
+
+
 def parse_config(config_file, base=None):
     config_file = XPath(config_file).absolute()
     config_base = config_file.parent
     with open(config_file) as cf:
         config = yaml.safe_load(cf)
+        
     config.setdefault("defaults", {})
     config["defaults"]["config_base"] = str(config_base)
     config["defaults"]["config_file"] = str(config_file)
@@ -18,12 +34,18 @@ def parse_config(config_file, base=None):
         config["defaults"]["dirs"]["base"] = base
 
     config = self_merge(config)
+    metafile = config['defaults'].get('benchmeta')
+    meta = load_meta(config_base, metafile)
 
     for name, defn in config["benchmarks"].items():
         defn.setdefault("name", name)
         defn.setdefault("group", name)
-        defn["tag"] = [defn["name"]]
-
+        
+        name = defn["name"]
+        defn["tag"] = [name]
+        
+        insert_meta(meta, defn)
+    
         pack = XPath(defn["definition"]).expanduser()
         if not pack.is_absolute():
             pack = (XPath(defn["config_base"]) / pack).resolve()
