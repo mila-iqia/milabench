1311536dfc7cdb108d4afda34553f9f820be377c
Setepenre <pierre.delaunay.tr@gmail.com>
2023-04-17 15:15:37 -0400

    Update run-rocm.bash

--- docker/run-rocm.bash
+++ docker/run-rocm.bash
@@ -1,12 +1,12 @@
 
-sudo docker run -it  --rm --shm-size=8G                        \
+sudo docker run -it  --rm --shm-size=32G                         \
         --device=/dev/kfd --device=/dev/dri                      \
         --security-opt seccomp=unconfined --group-add video      \
         -v $(pwd)/results:/milabench/envs/runs                   \
         ghcr.io/mila-iqia/milabench:rocm-nightly                 \
         milabench run
 
-sudo docker run -it  --rm --shm-size=8G                        \
+sudo docker run -it  --rm --shm-size=32G                         \
         --device=/dev/kfd --device=/dev/dri                      \
         --security-opt seccomp=unconfined --group-add video      \
         -v $(pwd)/results:/milabench/envs/runs                   \
