db495f45dc33a2a9f469db2fb1911005dc57538c
Setepenre <pierre.delaunay.tr@gmail.com>
2023-04-17 14:30:17 -0400

    Update run-cuda.bash

--- docker/run-cuda.bash
+++ docker/run-cuda.bash
@@ -2,14 +2,14 @@
 sudo docker pull ghcr.io/mila-iqia/milabench:cuda-nightly
 
 # Run milabench
-sudo docker run -it --rm --shm-size=1G                \
+sudo docker run -it --rm --shm-size=8G                \
       --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all  \
       -v $(pwd)/results:/milabench/envs/runs          \
       ghcr.io/mila-iqia/milabench:cuda-nightly        \
       milabench run
 
 # Show Performance Report
-sudo docker run -it --rm --shm-size=1G                \
+sudo docker run -it --rm --shm-size=8G                \
       --runtime=nvidia -e NVIDIA_VISIBLE_DEVICES=all  \
       -v $(pwd)/results:/milabench/envs/runs          \
       ghcr.io/mila-iqia/milabench:cuda-nightly        \
