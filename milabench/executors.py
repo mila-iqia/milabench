2a9e495fac8464f9a7ea5f93df590f371e57fce7
Satya Ortiz-Gagne <satya.ortiz-gagne@mila.quebec>
2023-06-06 14:01:59 -0400

    Add execution plan abstraction

---
+++ milabench/executors.py
@@ -0,0 +1,385 @@
+import asyncio
+from hashlib import md5
+import json
+from typing import Dict, Generator, List, Tuple, Union
+
+from .fs import XPath
+from .multi import clone_with
+from .pack import BasePackage
+from .alt_async import destroy
+
+from voir.instruments.gpu import get_gpu_info
+
+
+class Executor():
+    def __init__(
+            self,
+            pack_or_exec:Union["Executor", BasePackage],
+            **kwargs
+    ) -> None:
+        if isinstance(pack_or_exec, Executor):
+            self.exec = pack_or_exec
+            self._pack = None
+        else:
+            self.exec = None
+            self._pack = pack_or_exec
+
+        self._kwargs = kwargs
+
+    @property
+    def pack(self) -> BasePackage:
+        if self._pack:
+            return self._pack
+        return self.exec.pack
+
+    def argv(self, **kwargs) -> List:
+        if self.exec:
+            return self._argv(**kwargs) + self.exec.argv(**kwargs)
+        return self._argv(**kwargs)
+
+    def kwargs(self, **kwargs) -> Dict:
+        kwargs = {**self._kwargs, **kwargs}
+        if self.exec:
+            return self.exec.kwargs(**kwargs)
+        return kwargs
+
+    def commands(self) -> Generator[Tuple[BasePackage, List, Dict], None, None]:
+        yield self.pack, self.argv(), self.kwargs()
+
+    async def execute(self, **kwargs):
+        coro = []
+
+        for pack, argv, _kwargs in self.commands():
+            coro.append(pack.execute(*argv, **{**_kwargs, **kwargs}))
+
+        return await asyncio.gather(*coro)
+
+    def _argv(self, **kwargs) -> List:
+        del kwargs
+        return []
+
+
+# Leafs
+class PackExecutor(Executor):
+    def __init__(
+            self,
+            pack:BasePackage,
+            *script_argv,
+            **kwargs
+    ) -> None:
+        if isinstance(pack, Executor):
+            raise ValueError(f"{self.__class__} does not accept nested"
+                             f" {Executor.__class__}")
+        super().__init__(
+            pack,
+            **kwargs
+        )
+
+        script = script_argv[:1]
+        if script and XPath(script[0]).exists:
+            script = script[0]
+            script_argv = script_argv[1:]
+        else:
+            script = None
+
+        self.script = script
+        self.script_argv = script_argv
+
+    def _argv(self, **kwargs) -> List:
+        del kwargs
+        main = self.script or self.pack.main_script
+        if not XPath(main).is_absolute():
+            main = str(self.pack.dirs.code / main)  # Could this lead any
+                                                    # unexpected path during
+                                                    # exec?
+
+        if not main.exists():
+            raise FileNotFoundError(
+                f"Cannot run main script because it does not exist: {main}"
+            )
+
+        if main.is_dir():
+            main = ["-m", main]
+        else:
+            main = [main]
+        return [*main, *self.script_argv]
+
+
+# Branches or Roots
+class DockerRunExecutor(Executor):
+    def __init__(
+            self,
+            executor:Executor,
+            image:str,
+            *docker_argv,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            executor,
+            **kwargs
+        )
+        self.image = image
+        self.docker_argv = docker_argv
+
+    def _argv(self, **kwargs) -> List:
+        del kwargs
+        argv = [
+            "docker",
+            "run", "-i", "--rm", 
+            "--network", "host",
+            "--privileged",
+            "--gpus", "all",
+            *self.docker_argv
+        ]
+        env = self.pack.make_env()
+        for var in ('MILABENCH_CONFIG', 'XDG_CACHE_HOME', 'OMP_NUM_THREADS'):
+            argv.append("--env")
+            argv.append(f"{var}='{env[var]}'")
+        argv.append(self.image)
+        argv.append(f"{self.pack.dirs.code / 'activator'}") # What does this do?
+                                                            # Should it be part
+                                                            # of DockerExec or
+                                                            # sub exec?
+        argv.append(f"{self.pack.dirs.venv}")   # What does this do? Should it
+                                                # be part of DockerExec or sub
+                                                # exec?
+        return argv
+
+
+class SSHExecutor(Executor):
+    def __init__(
+            self,
+            executor:Executor,
+            host:str,
+            *ssh_argv,
+            user:str=None,
+            key:str=None,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            executor,
+            **kwargs
+        )
+        self.host = host
+        self.ssh_argv = list(*ssh_argv)
+        if user:
+            self.ssh_argv.append(f"-l{user}")
+        if key:
+            self.ssh_argv.append(f"-i{key}")
+
+    def _argv(self, **kwargs) -> List:
+        del kwargs
+        return [
+            "ssh",
+            "-oCheckHostIP=no",
+            "-oStrictHostKeyChecking=no",
+            *self.ssh_argv,
+            self.host,
+        ]
+
+
+class VoirExecutor(Executor):
+    def __init__(
+            self,
+            executor:Executor,
+            *voir_argv,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            executor,
+            **{"setsid":True, **kwargs}
+        )
+        self.voir_argv = voir_argv
+
+    def _argv(self, **kwargs) -> List:
+        if voirconf := self.pack.config.get("voir", None):
+            hsh = md5(str(voirconf).encode("utf8"))
+            voirconf_file = (
+                self.dirs.extra / f"voirconf-{self.tag}-{hsh.hexdigest()}.json"
+            )
+            with open(voirconf_file, "w") as f:
+                json.dump(fp=f, obj=voirconf, indent=4)
+            voir_argv = ("--config", voirconf_file)
+        else:
+            voir_argv = ()
+
+        return [
+            "voir",
+            *voir_argv,
+            *self.voir_argv,    # Is this going to cause errors? Should we only
+                                # *us voir_argv and remove self.voir_argv from
+                                # class?
+        ]
+
+
+# Roots
+class TimeOutExecutor(Executor):
+    def __init__(
+            self,
+            executor:Executor,
+            delay: int = 600,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            executor,
+            **kwargs
+        )
+        self.delay = delay
+
+    async def execute(self):
+        """Execute with timeout"""
+        async def force_terminate(pack, delay):
+            await asyncio.sleep(delay)
+            for proc in pack.processes:
+                ret = proc.poll()
+                if ret is None:
+                    await pack.message(
+                        f"Terminating process because it ran for longer than {delay} seconds."
+                    )
+                    destroy(proc)
+
+        coro = []
+
+        for pack, argv, kwargs in self.commands():
+            coro.append(pack.execute(*argv, **kwargs))
+            
+            asyncio.create_task(
+                force_terminate(
+                    pack, pack.config.get("max_duration", self.delay)
+                )
+            )
+
+        return await asyncio.gather(*coro)
+
+
+class NJobs(Executor):
+    """Runs n instance of the same job in parallel"""
+    def __init__(
+            self,
+            executor:Executor,
+            n:int,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            executor,
+            **kwargs
+        )
+        self.n = n
+
+    def commands(self) -> Generator[Tuple[BasePackage, List, Dict], None, None]:
+        for _ in range(self.n):
+            yield from super().commands()
+
+
+class PerGPU(Executor):
+    def __init__(
+            self,
+            executor:Executor,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            executor,
+            **kwargs
+        )
+
+    def commands(self) -> Generator[Tuple[BasePackage, List, Dict], None, None]:
+        gpus = get_gpu_info()["gpus"].values()
+        ngpus = len(gpus)
+        devices = gpus or [{
+            "device": 0,
+            "selection_variable": "CPU_VISIBLE_DEVICE"
+        }]
+
+        for gpu in devices:
+            gid = gpu["device"]
+            gcfg = {
+                "tag": [*self.exec.pack.config["tag"], f"D{gid}"],
+                "device": gid,
+                "devices": [gid] if ngpus else [],
+                "env": {gpu["selection_variable"]: str(gid)},
+            }
+            run = clone_with(self.exec.pack.config, gcfg)
+            run_pack = self.exec.pack.copy(run)
+            yield run_pack, self.argv(), self.kwargs()
+
+
+# Accelerate
+class AccelerateLaunchExecutor(Executor):
+    def __init__(
+            self,
+            pack:BasePackage,
+            *accelerate_argv,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            pack,
+            **kwargs
+        )
+        self.accelerate_argv = accelerate_argv
+
+    def _argv(self, rank, **kwargs) -> List:
+        del kwargs
+        nproc = len(self.pack.config.get("devices", [])) * self.pack.config['num_machines']
+        deepspeed_argv = [
+            "--use_deepspeed",
+            "--deepspeed_multinode_launcher=standard",
+            "--zero_stage=2",
+        ] if self.pack.config["use_deepspeed"] else ["--multi_gpu"]
+        return [
+            "accelerate",
+            "launch",
+            "--mixed_precision=fp16",
+            "--dynamo_backend=no",
+            f"--machine_rank={rank}",
+            f"--num_machines={self.pack.config['num_machines']}",
+            *deepspeed_argv,
+            f"--gradient_accumulation_steps={self.pack.config['gradient_accumulation_steps']}",
+            f"--num_cpu_threads_per_process={self.pack.config['cpus_per_gpu']}",
+            f"--main_process_ip={self.pack.config['manager_addr']}",
+            f"--main_process_port={self.pack.config['manager_port']}",
+            f"--num_processes={nproc}",
+            *self.accelerate_argv,
+            str(self.pack.dirs.code / "main.py")
+        ]
+
+
+class AccelerateLoopExecutor(Executor):
+    class _Placeholder(Executor):
+        def __init__(self) -> None:
+            pass
+
+    PLACEHOLDER=_Placeholder()
+
+    def __init__(
+            self,
+            accelerate_exec:AccelerateLaunchExecutor,
+            executor:SSHExecutor=None,
+            **kwargs
+    ) -> None:
+        if not isinstance(executor, SSHExecutor):
+            raise ValueError(f"{self.__class__} only accepts"
+                             f" {SSHExecutor.__class__} as nested"
+                             f" {Executor.__class__}")
+        super().__init__(
+            executor,
+            **kwargs
+        )
+        self.accelerate_exec = accelerate_exec
+        _exec = self
+        while _exec:
+            if _exec.exec is AccelerateLoopExecutor.PLACEHOLDER:
+                _exec.exec = self.accelerate_exec
+            _exec = _exec.exec
+
+    def commands(self) -> Generator[Tuple[BasePackage, List, Dict], None, None]:
+        yield (
+            self.pack,
+            self.accelerate_exec.argv(rank=0),
+            {"setsid":True, "use_stdout":True, **self.kwargs()}
+        )
+        for i, worker in enumerate(self.pack.config.get('worker_addrs', [])):
+            self.exec.host = worker
+            run_pack = self.pack.copy({
+                "tag": [*self.pack.config['tag'], worker]
+            })
+            yield run_pack, self.argv(rank=i + 1), self.kwargs()
---
+++ tests/test_executors.py
@@ -0,0 +1,179 @@
+import asyncio
+import os
+
+from milabench.executors import Executor, PackExecutor, VoirExecutor, NJobs, TimeOutExecutor
+from milabench.pack import Package
+from milabench.cli import _get_multipack
+from milabench.alt_async import proceed
+
+class MockPack(Package):
+    async def execute(self, *args, **kwargs):
+        return (
+            *args,
+            *[f"{k}:{v}" for k, v in kwargs.items()]
+        )
+
+class ExecMock1(Executor):
+    def __init__(
+            self,
+            pack_or_exec,
+            *exec_argv,
+            **kwargs
+    ) -> None:
+        super().__init__(
+            pack_or_exec,
+            **kwargs
+        )
+        self.exec_argv = exec_argv
+
+    def _argv(self, **kwargs):
+        return [
+            f"cmd{self.__class__.__name__}",
+            *self.exec_argv,
+            *[f"{self.__class__.__name__}[arg{i}]" for i in range(2)],
+            *[f"{self.__class__.__name__}{k}:{v}" for k, v in kwargs.items()]
+        ]
+
+class ExecMock2(ExecMock1):
+    pass
+
+TEST_FOLDER = os.path.dirname(__file__)
+
+
+def benchio():
+    packs = _get_multipack(
+        os.path.join(TEST_FOLDER, 'config', 'benchio.yaml'),
+        base='/tmp',
+        use_current_env=True,
+    )
+
+    _, pack = packs.packs.popitem()
+    return pack
+
+
+def mock_pack(pack):
+    async def execute(*args, **kwargs):
+        return [
+            *args,
+            *[f"{k}:{v}" for k, v in kwargs.items()]
+        ]
+    mock = pack
+    mock.execute = execute
+    return mock
+
+
+def test_executor_argv():
+    submock = ExecMock1(None, "a1", "a2")
+    wrapmock = ExecMock2(submock, "a3")
+
+    assert (
+        wrapmock.argv() ==
+        ["cmdExecMock2", "a3", "ExecMock2[arg0]", "ExecMock2[arg1]",
+         "cmdExecMock1", "a1", "a2", "ExecMock1[arg0]", "ExecMock1[arg1]"]
+    )
+
+    submock = ExecMock2(None)
+    wrapmock = ExecMock1(submock)
+
+    assert (
+        wrapmock.argv(k1="v1") ==
+        ["cmdExecMock1", "ExecMock1[arg0]", "ExecMock1[arg1]", "ExecMock1k1:v1",
+         "cmdExecMock2", "ExecMock2[arg0]", "ExecMock2[arg1]", "ExecMock2k1:v1"]
+    )
+
+
+def test_executor_kwargs():
+    submock = ExecMock1(None, selfk1="sv1", selfk2="sv2")
+    wrapmock = ExecMock2(submock, selfk1="sv1'", selfk3="sv3")
+    kwargs = {"selfk2":"v2''", "selfk3":"v3''", "k4":"v4"}
+
+    assert (
+        sorted(wrapmock.kwargs().keys()) == ["selfk1", "selfk2", "selfk3"]
+    )
+    assert (
+        sorted(wrapmock.kwargs().values()) == ["sv1'", "sv2", "sv3"]
+    )
+    assert (
+        sorted(
+            wrapmock.kwargs(**kwargs).keys()
+        ) == ["k4", "selfk1", "selfk2", "selfk3"]
+    )
+    assert (
+        sorted(
+            wrapmock.kwargs(**kwargs).values()
+        ) == ["sv1'", "v2''", "v3''", "v4"]
+    )
+
+
+def test_executor_execute():
+    submock = ExecMock1(mock_pack(benchio()), "a1", selfk1="sv1")
+    wrapmock = ExecMock2(submock, "a2", selfk2="sv2")
+
+    assert (
+        asyncio.run(wrapmock.execute(k3="v3")) ==
+        [["cmdExecMock2", "a2", "ExecMock2[arg0]", "ExecMock2[arg1]",
+          "cmdExecMock1", "a1", "ExecMock1[arg0]", "ExecMock1[arg1]",
+          "selfk1:sv1", "selfk2:sv2", "k3:v3"]]
+    )
+
+
+def test_pack_executor():
+    # voir is not setup so we are not receiving anything
+    executor = PackExecutor(benchio(), "--start", "2", "--end", "20")
+
+    acc = 0
+    for r in proceed(executor.execute()):
+        print(r)
+        acc += 1
+
+    assert acc == 2, "Only two message received"
+
+
+def test_voir_executor():
+    executor = PackExecutor(benchio(), "--start", "2", "--end", "20")
+    voir = VoirExecutor(executor)
+
+    acc = 0
+    for r in proceed(voir.execute()):
+        print(r)
+        acc += 1
+
+    assert  acc == 70
+
+
+def test_timeout():
+    executor = PackExecutor(benchio(), "--start", "2", "--end", "20", "--sleep", 20)
+    voir = VoirExecutor(executor)
+    timed = TimeOutExecutor(voir, delay=1)
+
+    acc = 0
+    for r in proceed(timed.execute()):
+        print(r)
+        acc += 1
+
+    assert acc > 2 and acc < 70 
+
+
+def test_njobs_executor():
+    executor = PackExecutor(benchio(), "--start", "2", "--end", "20")
+    voir = VoirExecutor(executor)
+    njobs = NJobs(voir, 5)
+
+    acc = 0
+    for r in proceed(njobs.execute()):
+        print(r)
+        acc += 1
+
+    assert acc == 70 * 5
+
+
+def test_njobs_novoir_executor():
+    executor = PackExecutor(benchio(), "--start", "2", "--end", "20")
+    njobs = NJobs(executor, 5)
+
+    acc = 0
+    for r in proceed(njobs.execute()):
+        print(r)
+        acc += 1
+
+    assert acc == 2 * 5
