a67fa0fa8b25ae257cb32116bb107d3fd652b731
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2023-07-04 15:08:38 -0400

    Tweaks

---
+++ milabench/capability.py
@@ -0,0 +1,27 @@
+from copy import deepcopy
+
+async def _failure(pack, condition):
+    msg = f"Skip {pack.config['name']} because the following capability is not satisfied: {condition}"
+    await pack.message(msg)
+
+
+async def is_system_capatible(pack):
+    # eval add __builtins__ to the dictionary, we copy it to not
+    # spoil our beautiful config
+    capability_context = deepcopy(pack.config["system"])
+    is_compatible = True
+
+    for condition in pack.config.get("requires_capabilities", []):
+        if not eval(condition, capability_context):
+            await _failure(pack, condition)
+            is_compatible = False
+
+    return is_compatible
+
+
+def sync_is_system_capatible(pack):
+    import asyncio
+
+    loop = asyncio.get_event_loop()
+    task = loop.create_task(is_system_capatible(pack))
+    return loop.run_until_complete(task)
