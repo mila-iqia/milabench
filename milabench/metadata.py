c46c4017a72c5d69bf581859004e388e8506c231
Setepenre <pierre.delaunay.tr@gmail.com>
2023-10-17 13:42:58 -0400

    Add pytorch version tracking

--- milabench/metadata.py
+++ milabench/metadata.py
@@ -8,6 +8,68 @@ from ._version import __commit__, __tag__, __date__
 from .vcs import retrieve_git_versions
 
 
+def get_pytorch_version():
+    
+    def clean(k: str):
+        pad = '  - '
+        if k.startswith(pad):
+            return k[len(pad):].strip()
+        return k.strip()
+        
+    def find_config(lines, key):
+        for line in lines:
+            if key in line:
+                return clean(line)
+        
+        return None
+        
+    def parse_build_settings(settings):
+        flags = dict()
+        
+        if settings is None:
+            return flags
+        
+        _, settings = settings.split(':')
+        for setting in settings.split(','):
+            try:
+                k, v = setting.split('=', maxsplit=1)
+                flags[k.strip()] = v.strip()
+            except ValueError:
+                pass
+                
+        return flags
+
+    try:
+        import torch
+        conf = torch.__config__.show().split('\n')
+
+        compiler = conf[1]
+        cpp = find_config(conf, 'C++ Version')
+        intel = find_config(conf, 'oneAPI')
+        mkl = find_config(conf, 'OpenMP')
+        openmp = find_config(conf, 'OpenMP')
+        lapack = find_config(conf, 'LAPACK')
+        nnpack = find_config(conf, 'NNPACK')
+        cpu = find_config(conf, 'CPU')
+        build_settings = find_config(conf, 'Build settings')
+
+        return dict(
+            torch=torch.__version__,
+            compiler=clean(compiler),
+            cpp=clean(cpp),
+            intel=clean(intel),
+            mkl=clean(mkl),
+            openmp=clean(openmp),
+            lapack=clean(lapack),
+            nnpack=clean(nnpack),
+            cpu=clean(cpu),
+            build_settings=parse_build_settings(build_settings)
+        )
+    
+    except ImportError:
+        return "NA"
+
+
 def machine_metadata():
     """Retrieve machine metadata"""
 
@@ -34,6 +96,5 @@ def machine_metadata():
             __commit__,
             __date__,
         ),
-        # How can we get pytorch version here
-        # "pytorch": 
+        "pytorch": get_pytorch_version()
     }
