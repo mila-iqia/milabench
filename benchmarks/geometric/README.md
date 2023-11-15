72f11ecfb75651be4ccf2677f9cc327275058ae0
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2023-11-15 14:12:51 -0500

    wip

---
+++ benchmarks/geometric/README.md
@@ -0,0 +1,6 @@
+
+
+https://github.com/DeepGraphLearning/torchdrug
+https://github.com/DeepGraphLearning/GNN-QE
+
+
---
+++ benchmarks/geometric/benchfile.py
@@ -0,0 +1,19 @@
+from milabench.pack import Package
+
+
+class FlopsBenchmarch(Package):
+    base_requirements = "requirements.in"
+    prepare_script = "prepare.py"
+    main_script = "main.py"
+    
+    def build_run_plan(self) -> "execs.Executor":
+        import milabench.executors as execs
+        
+        main = self.dirs.code / self.main_script
+        pack = execs.PackExecutor(self, *self.argv, lazy=True)
+        # pack = execs.VoirExecutor(pack, cwd=main.parent)
+        pack = execs.ActivatorExecutor(pack, use_stdout=True)
+        return pack
+    
+
+__pack__ = FlopsBenchmarch
