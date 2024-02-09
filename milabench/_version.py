4d1c3320d8a88764868317340c56976c70471d71
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2024-02-09 15:23:07 -0500

    Add new dependency

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v0.0.6-55-gc0a93608"
-__commit__ = "c0a93608db6511fe696fd044ccde3891bd1b57ed"
-__date__ = "2024-02-09 12:57:17 -0500"
+__tag__ = "v0.0.6-56-g83ff7c5d"
+__commit__ = "83ff7c5d762bdc34d6c0aeeb514dfc3ccb3b7eb4"
+__date__ = "2024-02-09 14:12:36 -0500"
