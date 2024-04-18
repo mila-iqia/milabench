9b8a3a0941855b0a239c7bb88831e6e464015be4
Your Name <you@example.com>
2024-04-18 18:35:13 +0000

    pin for xpu

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v0.0.6-41-g932e30e"
-__commit__ = "932e30e79513fdd2448cedaf98a003bb4b5b9148"
-__date__ = "2024-01-17 14:33:14 -0500"
+__tag__ = "v0.0.6-49-g4975328"
+__commit__ = "4975328408e2ad54fe448d2881c6aca8fe200fd2"
+__date__ = "2024-04-18 18:16:53 +0000"
