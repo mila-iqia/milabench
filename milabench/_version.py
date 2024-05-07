ad1618e1b3569059a2986dfcb00bd906ae4c04c3
Your Name <you@example.com>
2024-05-07 19:42:13 +0000

    add HPU requirements

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v0.0.6-52-g5cb5067"
-__commit__ = "5cb5067d32c6ec60607fff594070b0575a563720"
-__date__ = "2024-04-18 19:08:05 +0000"
+__tag__ = "v0.0.6-64-g1ffa65d"
+__commit__ = "1ffa65db29b76fc12f37499c5dfee572488a7083"
+__date__ = "2024-05-07 12:08:01 -0400"
