41bd19598b939b6d62e104c3f3a64b57faf9e06f
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2024-05-30 15:16:20 -0400

    Rework data generation

--- benchmarks/torchvision/prepare.py
+++ benchmarks/torchvision/prepare.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 
+import argparse
 import multiprocessing
 import os
 from pathlib import Path
@@ -10,32 +11,38 @@ from tqdm import tqdm
 
 
 def write(args):
-    from torchvision.datasets import FakeData
+    import torch 
+    import torchvision.transforms as transforms
 
-    image_size, offset, count, outdir = args
-    dataset = FakeData(
-        size=count, image_size=image_size, num_classes=1000, random_offset=offset
-    )
+    offset, outdir, size  = args
 
-    image, y = next(iter(dataset))
-    class_val = int(y)
+    img = torch.randn(*size)
+    target = torch.randint(0, 1000, size=(1,), dtype=torch.long)[0]
+    img = transforms.ToPILImage()(img)
+
+    class_val = int(target)
     image_name = f"{offset}.jpeg"
 
     path = os.path.join(outdir, str(class_val))
     os.makedirs(path, exist_ok=True)
 
     image_path = os.path.join(path, image_name)
-    image.save(image_path)
+    img.save(image_path)
 
 
 def generate(image_size, n, outdir):
-    p_count = min(multiprocessing.cpu_count(), 8)
-    pool = multiprocessing.Pool(p_count)
-    for _ in tqdm(
-        pool.imap_unordered(write, ((image_size, i, n, outdir) for i in range(n))),
-        total=n,
-    ):
-        pass
+    work_items = []
+    for i in range(n):
+        work_items.append([
+            i,
+            outdir,
+            image_size,
+        ])
+
+    n_worker = min(multiprocessing.cpu_count(), 8)
+    with multiprocessing.Pool(n_worker) as pool:
+        for _ in tqdm(pool.imap_unordered(write, work_items), total=n):
+            pass
 
 
 def generate_sets(root, sets, shape):
@@ -53,19 +60,29 @@ def generate_sets(root, sets, shape):
     sentinel.touch()
 
 
-if __name__ == "__main__":
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--batch-size", default=512, type=int)
+    parser.add_argument("--batch-count", default=60, type=int)
+    parser.add_argument("--image-size", default=[3, 384, 384], type=int, nargs="+")
+    parser.add_argument("--val", default=0.1, type=float, nargs="+")
+    parser.add_argument("--test", default=0.1, type=float, nargs="+")
+    args, _ = parser.parse_known_args()
+
     data_directory = os.environ["MILABENCH_DIR_DATA"]
     dest = os.path.join(data_directory, "FakeImageNet")
     print(f"Generating fake data into {dest}...")
 
-    batch_size = 1024
-    observation_required = 60
-    total_images = batch_size * observation_required
+    total_images = args.batch_size * args.batch_count
     size_spec = {
         "train": total_images, 
-        "val": int(total_images * 0.1), 
-        "test": int(total_images * 0.1)
+        "val": int(total_images * args.val), 
+        "test": int(total_images * args.test)
     }
 
-    generate_sets(dest, size_spec, (3, 384, 384))
+    generate_sets(dest, size_spec, args.image_size)
     print("Done!")
+
+
+if __name__ == "__main__":
+    main()
