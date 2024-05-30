f45dd139f5f1026f548272bb18f99c47ec94bd36
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2024-05-30 12:40:24 -0400

    Tweak batch resize computation

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v0.0.6-82-g49a6458"
-__commit__ = "49a645807e5449e4d9ce4d11867515edbc6ae638"
-__date__ = "2024-05-21 17:41:06 +0000"
+__tag__ = "v0.0.6-89-gdd9ece6"
+__commit__ = "dd9ece6fc8b68ace8de2841284798641c96b85bf"
+__date__ = "2024-05-23 15:02:40 +0000"
