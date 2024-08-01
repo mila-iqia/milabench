c31e7be9b01fffeb8f1461761cc114b111ffdd08
Pierre Delaunay <pierre@delaunay.io>
2024-08-01 12:14:45 -0400

    Simplify requirements.in

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "3a30894"
-__commit__ = "3a30894b44bc570983fb3f19bb316babddeb83f2"
-__date__ = "2024-08-01 11:06:49 -0400"
+__tag__ = "v0.0.6-80-ga6bb275"
+__commit__ = "a6bb275e184d99ddf5c35feaaeba82b77a0b2cea"
+__date__ = "2024-08-01 11:27:52 -0400"
