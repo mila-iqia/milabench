a6bb275e184d99ddf5c35feaaeba82b77a0b2cea
roger-creus <creus99@gmail.com>
2024-08-01 11:27:52 -0400

    Initial commit Torch_PPO_Cleanrl_Atari_Envpool

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v0.1.0-30-g64aa548b"
-__commit__ = "64aa548ba07d3c6bb298e435b8ac43c69eb75738"
-__date__ = "2024-07-26 13:07:25 -0400"
+__tag__ = "3a30894"
+__commit__ = "3a30894b44bc570983fb3f19bb316babddeb83f2"
+__date__ = "2024-08-01 11:06:49 -0400"
