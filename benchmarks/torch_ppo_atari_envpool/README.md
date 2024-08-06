eed157a509567149d16d23bd4d721bc4cd547df3
Roger Creus <31919499+roger-creus@users.noreply.github.com>
2024-08-06 11:19:11 -0400

    Initial commit Torch_PPO_Cleanrl_Atari_Envpool (#243)
    * Initial commit Torch_PPO_Cleanrl_Atari_Envpool
    
    * Simplify requirements.in
    
    * Code now runs
    
    * instrumentation concept
    
    * Fix CPU scaling
    
    ---------
    
    Co-authored-by: Pierre Delaunay <pierre@delaunay.io>

---
+++ benchmarks/torch_ppo_atari_envpool/README.md
@@ -0,0 +1,4 @@
+
+# Torch_ppo_atari_envpool
+
+Rewrite this README to explain what the benchmark is!
--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,7 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v0.1.0-38-gfb01d691"
-__commit__ = "fb01d691aa0d88717dcb3fea8852f61e111cc75f"
-__date__ = "2024-08-01 18:59:13 -0400"
+
+__tag__ = "c7ae304"
+__commit__ = "c7ae3043a12faef4da3eb0ddd6dc33e355b265fc"
+__date__ = "2024-08-01 17:03:10 -0400"
+
