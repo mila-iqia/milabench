321acb6dd19fcbb8231e6da21a0afe8289be9a49
Your Name <you@example.com>
2024-08-21 17:22:22 +0000

    update rocm dependencies

--- scripts/article/run_rocm.sh
+++ scripts/article/run_rocm.sh
@@ -43,6 +43,22 @@ install_prepare() {
         pip uninstall torch torchvision torchaudio
         pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
         pip uninstall pynvml
+
+        # sudo apt-get install lld
+        # https://github.com/ROCm/jax/releases/tag/rocm-jaxlib-v0.4.30
+        # does not really work
+        pip install https://github.com/ROCm/jax/releases/download/rocm-jaxlib-v0.4.30/jaxlib-0.4.30+rocm611-cp310-cp310-manylinux2014_x86_64.whl
+        pip install https://github.com/ROCm/jax/archive/refs/tags/rocm-jaxlib-v0.4.30.tar.gz
+
+        # 
+        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_cluster.git
+        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
+        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_sparse.git
+
+        # takes forever to compile
+        # https://github.com/ROCm/xformers
+        pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
+        pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/flash-attention.git
     )
 
     #
