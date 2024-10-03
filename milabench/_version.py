046308f54b06349abd0fdbefbadd87c3b28f4dfb
Your Name <you@example.com>
2024-10-03 17:25:42 +0000

    -

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.0.0_RC1-12-g6691bc8"
-__commit__ = "6691bc86d8917a7ea89308fb99dc3838912e9a9b"
-__date__ = "2024-10-03 14:52:58 +0000"
+__tag__ = "v1.0.0_RC1-13-gde92a7e"
+__commit__ = "de92a7ea9dea1da24e8105e4566d5e6daef8464c"
+__date__ = "2024-10-03 15:48:10 +0000"
--- scripts/article/run_rocm.sh
+++ scripts/article/run_rocm.sh
@@ -2,6 +2,10 @@
 
 set -ex
 
+
+# sudo usermod -a -G render,video $LOGNAME
+# sudo chmod u+s /opt/rocm-6.2.2/lib/llvm/bin/amdgpu-arch
+
 export MILABENCH_GPU_ARCH=rocm
 export MILABENCH_WORDIR="$(pwd)/$MILABENCH_GPU_ARCH"
 
@@ -50,6 +54,8 @@ install_prepare() {
     (
         . $BENCHMARK_VENV/bin/activate
 
+        pip install ninja
+
         if [ -z "${MILABENCH_HF_TOKEN}" ]; then
             echo "Missing token"
         else
@@ -78,7 +84,7 @@ install_prepare() {
         # takes forever to compile
         # https://github.com/ROCm/xformers
         pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
-        pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/flash-attention.git 
+        pip install -v -U --no-build-isolation --use-pep517 --no-deps git+https://github.com/ROCm/flash-attention.git 
     )
 
     #
@@ -95,6 +101,47 @@ else
 fi
 
 cd $MILABENCH_WORDIR
+
+
+    (
+        . $BENCHMARK_VENV/bin/activate
+
+        pip install ninja
+
+        if [ -z "${MILABENCH_HF_TOKEN}" ]; then
+            echo "Missing token"
+        else
+            huggingface-cli login --token $MILABENCH_HF_TOKEN
+        fi
+
+        #
+        # Override/add package to the benchmark venv here
+        #
+        which pip
+        # pip uninstall torch torchvision torchaudio -y
+        # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
+        # pip uninstall pynvml
+
+        # sudo apt-get install lld
+        # https://github.com/ROCm/jax/releases/tag/rocm-jaxlib-v0.4.30
+        # does not really work
+        pip install https://github.com/ROCm/jax/releases/download/rocm-jaxlib-v0.4.30/jaxlib-0.4.30+rocm611-cp310-cp310-manylinux2014_x86_64.whl
+        pip install https://github.com/ROCm/jax/archive/refs/tags/rocm-jaxlib-v0.4.30.tar.gz
+
+        # 
+        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_cluster.git
+        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
+        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_sparse.git
+
+        # takes forever to compile
+        # https://github.com/ROCm/xformers
+        pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
+        pip install -v -U --no-build-isolation --use-pep517 --no-deps git+https://github.com/ROCm/flash-attention.git 
+    )
+
+. $MILABENCH_WORDIR/env/bin/activate
+
+milabench prepare $ARGS 
 
 #
 #   Run the benchmakrs
