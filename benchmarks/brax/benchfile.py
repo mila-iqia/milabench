b58e5b15a1c7e6e601b31ce64c0f630afb3d2057
Your Name <you@example.com>
2024-10-07 19:22:15 +0000

    Cleanup the rocm script

--- benchmarks/brax/benchfile.py
+++ benchmarks/brax/benchfile.py
@@ -5,5 +5,9 @@ class BraxBenchmark(Package):
     base_requirements = "requirements.in"
     main_script = "main.py"
 
-
+    def make_env(self):
+        env = super().make_env()
+        env["XLA_PYTHON_CLIENT_PREALLOCATE"] = "False"
+        return env
+    
 __pack__ = BraxBenchmark
--- scripts/article/run_rocm.sh
+++ scripts/article/run_rocm.sh
@@ -2,13 +2,12 @@
 
 set -ex
 
-
 # sudo usermod -a -G render,video $LOGNAME
 # sudo chmod u+s /opt/rocm-6.2.2/lib/llvm/bin/amdgpu-arch
 
 export MILABENCH_GPU_ARCH=rocm
 export MILABENCH_WORDIR="$(pwd)/$MILABENCH_GPU_ARCH"
-
+export ROCM_PATH="/opt/rocm"
 export MILABENCH_BASE="$MILABENCH_WORDIR/results"
 export MILABENCH_VENV="$MILABENCH_WORDIR/env"
 export BENCHMARK_VENV="$MILABENCH_WORDIR/results/venv/torch"
@@ -66,27 +65,27 @@ install_prepare() {
         # Override/add package to the benchmark venv here
         #
         which pip
-        # pip uninstall torch torchvision torchaudio -y
-        # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
-        # pip uninstall pynvml
 
-        # sudo apt-get install lld
         # https://github.com/ROCm/jax/releases/tag/rocm-jaxlib-v0.4.30
-        # does not really work
         pip install https://github.com/ROCm/jax/releases/download/rocm-jaxlib-v0.4.30/jaxlib-0.4.30+rocm611-cp310-cp310-manylinux2014_x86_64.whl
         pip install https://github.com/ROCm/jax/archive/refs/tags/rocm-jaxlib-v0.4.30.tar.gz
 
         # 
-        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_cluster.git
-        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
-        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_sparse.git
+        FORCE_ONLY_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_cluster.git
+        FORCE_ONLY_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
+        FORCE_ONLY_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_sparse.git
 
         # takes forever to compile
         # https://github.com/ROCm/xformers
-        pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
+        pip install xformers --index-url https://download.pytorch.org/whl/rocm6.1
+        # pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
+        # pip install -v -U --no-build-isolation --no-deps git+https://github.com/facebookresearch/xformers.git
+        # pip install xformers -U --index-url https://download.pytorch.org/whl/rocm6.1
         pip install -v -U --no-build-isolation --use-pep517 --no-deps git+https://github.com/ROCm/flash-attention.git 
+        pip uninstall pynvml nvidia-ml-py -y
     )
 
+    pip uninstall pynvml nvidia-ml-py -y
     #
     #   Generate/download datasets, download models etc...
     #
@@ -101,48 +100,6 @@ else
 fi
 
 cd $MILABENCH_WORDIR
-
-
-    (
-        . $BENCHMARK_VENV/bin/activate
-
-        pip install ninja
-
-        if [ -z "${MILABENCH_HF_TOKEN}" ]; then
-            echo "Missing token"
-        else
-            huggingface-cli login --token $MILABENCH_HF_TOKEN
-        fi
-
-        #
-        # Override/add package to the benchmark venv here
-        #
-        which pip
-        # pip uninstall torch torchvision torchaudio -y
-        # pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/rocm6.1
-        # pip uninstall pynvml
-
-        # sudo apt-get install lld
-        # https://github.com/ROCm/jax/releases/tag/rocm-jaxlib-v0.4.30
-        # does not really work
-        pip install https://github.com/ROCm/jax/releases/download/rocm-jaxlib-v0.4.30/jaxlib-0.4.30+rocm611-cp310-cp310-manylinux2014_x86_64.whl
-        pip install https://github.com/ROCm/jax/archive/refs/tags/rocm-jaxlib-v0.4.30.tar.gz
-
-        # 
-        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_cluster.git
-        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
-        FORCE_CUDA=1 pip install -U -v --no-build-isolation git+https://github.com/rusty1s/pytorch_sparse.git
-
-        # takes forever to compile
-        # https://github.com/ROCm/xformers
-        pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
-        pip install -v -U --no-build-isolation --use-pep517 --no-deps git+https://github.com/ROCm/flash-attention.git 
-    )
-
-. $MILABENCH_WORDIR/env/bin/activate
-
-milabench prepare $ARGS 
-
 #
 #   Run the benchmakrs
 milabench run $ARGS 
