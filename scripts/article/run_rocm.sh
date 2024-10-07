db191141a0373aa81ccf5152c7c358ea43000a2d
Your Name <you@example.com>
2024-10-07 19:22:53 +0000

    use rocm branch

--- scripts/article/run_rocm.sh
+++ scripts/article/run_rocm.sh
@@ -28,7 +28,7 @@ install_prepare() {
 
     if [ -z "${MILABENCH_SOURCE}" ]; then
         if [ ! -d "$MILABENCH_WORDIR/milabench" ]; then
-            git clone https://github.com/mila-iqia/milabench.git
+            git clone https://github.com/mila-iqia/milabench.git -b rocm
         fi
         export MILABENCH_SOURCE="$MILABENCH_WORDIR/milabench"
     fi
