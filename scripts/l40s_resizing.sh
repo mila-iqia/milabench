61ca0a328fe57ed5aca60c11506dc57e85a8acbb
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2025-02-04 12:11:28 -0500

    Set right version for cantilever

--- scripts/l40s_resizing.sh
+++ scripts/l40s_resizing.sh
@@ -5,5 +5,6 @@
 conda activate py310
 
 rm -rf /tmp/workspace/runs
-CUDA_VISIBLE_DEVICES=0 milabench run --system /tmp/workspace/system.yaml --base /tmp/workspace --config /home/mila/d/delaunap/scratch/shared/milabench/config/standard.yaml --select resnet50
-
+# CUDA_VISIBLE_DEVICES=0 
+export MILABENCH_SIZER_CONFIG=/home/mila/d/delaunap/scratch/shared/milabench/config/scaling/L40S.yaml
+milabench run --system /tmp/workspace/system.yaml --base /tmp/workspace --config /home/mila/d/delaunap/scratch/shared/milabench/config/standard.yaml --exclude nobatch > out.txt
