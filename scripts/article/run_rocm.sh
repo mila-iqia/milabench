47486cf079a6d07571f07183f3ae7178fec3c1c3
Your Name <you@example.com>
2025-02-18 15:19:35 +0000

    MI325 - rocm 6.2

--- scripts/article/run_rocm.sh
+++ scripts/article/run_rocm.sh
@@ -5,6 +5,10 @@ set -ex
 # sudo usermod -a -G render,video $LOGNAME
 # sudo chmod u+s /opt/rocm-6.2.2/lib/llvm/bin/amdgpu-arch
 
+
+# sudo apt-get install g++-12 libstdc++-12-dev
+
+
 export MILABENCH_GPU_ARCH=rocm
 export MILABENCH_WORDIR="$(pwd)/$MILABENCH_GPU_ARCH"
 export ROCM_PATH="/opt/rocm"
@@ -84,25 +88,28 @@ install_prepare() {
         # Override/add package to the benchmark venv here
         #
         which pip
+        export MAX_JOBS=24
+        # https://github.com/ROCm/jax/releases/download/rocm-jax-v0.5.0/jaxlib-0.5.0-cp310-cp310-manylinux_2_28_x86_64.whl
 
         # https://github.com/ROCm/jax/releases/tag/rocm-jaxlib-v0.4.30
-        pip install https://github.com/ROCm/jax/releases/download/rocm-jaxlib-v0.4.30/jaxlib-0.4.30+rocm611-cp310-cp310-manylinux2014_x86_64.whl
-        pip install https://github.com/ROCm/jax/archive/refs/tags/rocm-jaxlib-v0.4.30.tar.g
+        # pip install https://github.com/ROCm/jax/releases/download/rocm-jaxlib-v0.4.30/jaxlib-0.4.30+rocm611-cp310-cp310-manylinux2014_x86_64.whl
+        # pip install https://github.com/ROCm/jax/archive/refs/tags/rocm-jaxlib-v0.4.30.tar.gz
 
-        pip uninstall torch_cluster torch_scatter torch_sparse -y
-        FORCE_ONLY_CUDA=1 pip install -U -v --use-pep517 --no-build-isolation git+https://github.com/rusty1s/pytorch_cluster.git
-        FORCE_ONLY_CUDA=1 pip install -U -v --use-pep517 --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
-        FORCE_ONLY_CUDA=1 pip install -U -v --use-pep517 --no-build-isolation git+https://github.com/rusty1s/pytorch_sparse.git
+        # pip uninstall torch_cluster torch_scatter torch_sparse -y
+        # FORCE_ONLY_CUDA=1 pip install -U -v --use-pep517 --no-build-isolation git+https://github.com/rusty1s/pytorch_cluster.git
+        # FORCE_ONLY_CUDA=1 pip install -U -v --use-pep517 --no-build-isolation git+https://github.com/rusty1s/pytorch_scatter.git
+        # FORCE_ONLY_CUDA=1 pip install -U -v --use-pep517 --no-build-isolation git+https://github.com/rusty1s/pytorch_sparse.git
 
         # takes forever to compile
         # https://github.com/ROCm/xformers
-        pip uninstall xformers
-        pip install xformers --index-url https://download.pytorch.org/whl/rocm6.1
-        # pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
+        pip uninstall xformers -y
+        # pip install "torch<2.6" --index-url https://download.pytorch.org/whl/rocm6.2
+        # pip install xformers==0.0.29 --index-url https://download.pytorch.org/whl/rocm6.2
+        pip install -v -U --no-build-isolation --no-deps git+https://github.com/ROCm/xformers.git@develop#egg=xformers
         # pip install -v -U --no-build-isolation --no-deps git+https://github.com/facebookresearch/xformers.git
         # pip install xformers -U --index-url https://download.pytorch.org/whl/rocm6.1
 
-        pip uninstall flash-attention
+        pip uninstall -y flash-attention
         pip install -v -U --no-build-isolation --use-pep517 --no-deps git+https://github.com/ROCm/flash-attention.git 
         pip uninstall pynvml nvidia-ml-py -y
 
@@ -123,20 +130,23 @@ else
     . $MILABENCH_WORDIR/env/bin/activate
 fi
 
-(
-    . $BENCHMARK_VENV/bin/activate
-    pip install xformers --index-url https://download.pytorch.org/whl/rocm6.1
-)
+# (
+#     # . $BENCHMARK_VENV/bin/activate
+#     # pip install xformers --index-url https://download.pytorch.org/whl/rocm6.1
+# )
 
 # milabench install $ARGS --system $MILABENCH_WORDIR/system.yaml
 
-# milabench prepare $ARGS --system $MILABENCH_WORDIR/system.yaml
+# milabench prepare $ARGS # --system $MILABENCH_WORDIR/system.yaml
+# install_prepare
 
 #
 #   Run the benchmakrs
-milabench run $ARGS --system $MILABENCH_WORDIR/system.yaml
+milabench run $ARGS # --system $MILABENCH_WORDIR/system.yaml
 
 
 #
 #   Display report
 milabench report --runs $MILABENCH_WORDIR/results/runs
+
+# rocm
