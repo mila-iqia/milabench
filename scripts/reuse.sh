21b96e791c6675d989f6717a039c041259930cd3
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2025-04-18 08:48:09 -0400

    Finalize L40S scaling data

--- scripts/reuse.sh
+++ scripts/reuse.sh
@@ -15,11 +15,11 @@ export MILABENCH_GPU_ARCH=cuda
 export MILABENCH_CONFIG=~/scratch/shared/milabench/config/standard.yaml
 export MILABENCH_MULTIRUN_CACHE=~/scratch/shared/progress.txt
 
-# milabench sharedsetup --network /home/mila/d/delaunap/scratch/shared/ --local $MILABENCH_BASE
+milabench sharedsetup --network /home/mila/d/delaunap/scratch/shared/ --local $MILABENCH_BASE
 
 
 # Those 2 commands should execute faster now (it will only verify)
-# MILABENCH_USE_UV=1 milabench install
+MILABENCH_USE_UV=1 milabench install
 # milabench prepare 
 
 # Runs
