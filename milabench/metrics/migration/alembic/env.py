908e4340229a91086b1aaff57f545e975da0dbc8
Pierre Delaunay <pierre@delaunay.io>
2025-07-31 13:11:02 -0400

    Update alembic config to take a URL from env

--- milabench/metrics/migration/alembic/env.py
+++ milabench/metrics/migration/alembic/env.py
@@ -1,4 +1,5 @@
 from logging.config import fileConfig
+import os
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
@@ -13,6 +14,14 @@ config = context.config
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+
+local_dev = "postgresql://milabench_write:1234@localhost:5432/milabench"
+
+db_url = os.getenv("DATABASE_URI", local_dev)
+
+# override the sqlalchemy.url config
+config.set_main_option("sqlalchemy.url", db_url)
 
 # add your model's MetaData object here
 # for 'autogenerate' support
