4212694d537f2bc5f23b512df8c1d5967b3ebd5e
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2025-08-11 13:42:55 -0400

    Pin

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.1.1-77-g6b97581"
-__commit__ = "6b975812aeb7b53c70961c373c09ddd2f386c4db"
-__date__ = "2025-07-08 15:36:06 -0400"
+__tag__ = "v1.1.1-101-gdb21f2f"
+__commit__ = "db21f2f7acfad9dfebbcb02992cdf2cd9f077270"
+__date__ = "2025-08-11 12:36:58 -0400"
