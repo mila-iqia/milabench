cfe7f50ef983c6be4ad4358a47547bf59abc815d
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2025-08-11 17:13:19 -0400

    Pin Cuda 3.12

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.1.1-101-g3eae526"
-__commit__ = "3eae526e37cdc54980866d4516664b195938b9dc"
-__date__ = "2025-08-11 14:09:41 -0400"
+__tag__ = "v1.1.1-102-g2e49a27"
+__commit__ = "2e49a27358daff5a104d0c751972cc0dec9c7fbc"
+__date__ = "2025-08-11 16:39:06 -0400"
