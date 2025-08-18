97c98d571dfb8ed191d5f3cc89659d418e9224f0
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2025-08-18 13:17:50 -0400

    Call the right parser

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.1.1-103-g896bbde"
-__commit__ = "896bbde44bb1247449dd248620b0778a402020c3"
-__date__ = "2025-08-12 12:41:26 -0400"
+__tag__ = "v1.1.1-113-gc37bfe5"
+__commit__ = "c37bfe570e84378f9598f57692ca32e26c100a07"
+__date__ = "2025-08-18 12:18:52 -0400"
