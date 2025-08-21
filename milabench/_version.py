d13e66caeafaadf9d8037d758cc11d4709f0e061
Setepenre <pierre.delaunay.tr@gmail.com>
2025-08-21 12:34:02 -0400

    py3.12_torch2.8+cu129 (#364)
    Co-authored-by: pierre.delaunay <delaunap@rtx5.server.mila.quebec>

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.1.1-113-gc37bfe5"
-__commit__ = "c37bfe570e84378f9598f57692ca32e26c100a07"
-__date__ = "2025-08-18 12:18:52 -0400"
+__tag__ = "v1.2.0-2-gf7b9171"
+__commit__ = "f7b91718841ef4bf3e7a047ab17483e59063d24f"
+__date__ = "2025-08-21 12:25:24 -0400"
