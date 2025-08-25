df6c9a95ebff289d1116c2c4e5030d50fcfc05e8
pierre.delaunay <delaunap@rtx5.server.mila.quebec>
2025-08-25 13:33:28 -0400

    Pin Dependencies

--- milabench/_version.py
+++ milabench/_version.py
@@ -1,5 +1,5 @@
 """This file is generated, do not modify"""
 
-__tag__ = "v1.2.0-2-gf7b9171"
-__commit__ = "f7b91718841ef4bf3e7a047ab17483e59063d24f"
-__date__ = "2025-08-21 12:25:24 -0400"
+__tag__ = "v1.2.0-8-g4c928a8"
+__commit__ = "4c928a8abf41e9725724558ff1b367611078716d"
+__date__ = "2025-08-25 13:29:05 -0400"
