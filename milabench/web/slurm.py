cd22037f022568a429dac12bbbb9aad4495c23ac
Pierre Delaunay <pierre.delaunay@outlook.com>
2025-08-27 14:17:22 -0400

    Pipeline run concept

--- milabench/web/slurm.py
+++ milabench/web/slurm.py
@@ -51,38 +51,6 @@ AFTER_NOT_OK = "afternotok"
 SINGLETON = "singleton"
 
 
-class Pipeline:
-    """The job runner define a dependency between jobs and schedule them to slurm.
-    It is able to launch a job on the cluster.
-
-    Its most interesting feature is being able to track which job failed and which were successful 
-    and only trigger the failed one.
-
-    To do so it simply iterate over the job tree and mark the one failed to retry and skip the successful ones.
-    """
-
-    def __init__(self, job_definition):
-        self.definition: JobNode = job_definition
-
-        # A pipeline run is something only known to the job runner and as such does not have Slurm Job ID
-        # But it has a jobs id
-        self.job_id = None
-
-    def schedule(self):
-        pass
-
-    def rerun(self) -> Pipeline:
-        # traverse the job definition and create a new one
-        pass
-
-    def __json__(self):
-        return {
-            "type": "pipeline",
-            "definition": self.definition.__json__(),
-            "job_id": self.job_id
-        }
- 
-
 class JobNode:
     def gen(self, depends_event=AFTER_OK, depends_on=None):
         pass
@@ -105,6 +73,51 @@ class JobNode:
                 raise RuntimeError(f"Unknown type: {unknown_type}")
 
 
+class Pipeline:
+    """The job runner define a dependency between jobs and schedule them to slurm.
+    It is able to launch a job on the cluster.
+
+    Its most interesting feature is being able to track which job failed and which were successful
+    and only trigger the failed one.
+
+    To do so it simply iterate over the job tree and mark the one failed to retry and skip the successful ones.
+
+    The pipeline runs alterate the folder structure to make the output fs reflect the job dependencies
+
+    """
+
+    def __init__(self, name, job_definition):
+        self.definition: JobNode = job_definition
+        self.name = name
+
+        # A pipeline run is something only known to the job runner and as such does not have Slurm Job ID
+        # But it has a jobs id
+        self.job_id = None
+
+    def output_dir(self, root=JOBRUNNER_WORKDIR):
+        return os.path.join(root, self.name)
+
+    def schedule(self):
+        context = {
+            "output": [self.output_dir()]
+        }
+
+        self.definition.gen(context)
+
+
+    def rerun(self) -> Pipeline:
+        # traverse the job definition and create a new one
+        pass
+
+    def __json__(self):
+        return {
+            "type": "pipeline",
+            "definition": self.definition.__json__(),
+            "job_id": self.job_id,
+            "name": self.name
+        }
+
+
 class Job(JobNode):
     def __init__(self, script, profile, job_id=None, slurm_jobid=None):
         self.script = script
@@ -112,16 +125,27 @@ class Job(JobNode):
         self.job_id = job_id
         self.slurm_jobid = slurm_jobid
 
-    def gen(self, depends_event=AFTER_OK, depends_on=None):
-        args = []
+    def output_dir(self, root=JOBRUNNER_WORKDIR):
+        return os.path.join(root, self.job_id)
+
+    def gen(self, context, depends_event=AFTER_OK, depends_on=None):
+        sbatch_args = []
 
         if depends_on is not None:
-            args.append(f"--dependency={depends_event}:{depends_on}")
+            sbatch_args.append(f"--dependency={depends_event}:{depends_on}")
 
-        # Here we submit the job definition to slurm itslef get the jobid from the query
-        # also make a job runner id for it
-        cmd = "squeue ..."
-    
+        # fetch resource profile
+        # ...
+
+        # fetch script
+        # ...
+
+        # Create the job folder locally and rsync it to remote
+
+        # Build the sbatch command
+
+        # Submit the job, get a slurm_id for dependencies if any
+
         return self.slurm_jobid
 
     def __json__(self):
@@ -135,8 +159,13 @@ class Job(JobNode):
 
 
 class Sequential(JobNode):
-    def __init__(self, *jobs):
+    def __init__(self, *jobs, name='S'):
+        self.name = name
+        self.job_id = None
         self.jobs = jobs
+
+    def output_dir(self, root):
+        return os.path.join(root, self.name)
 
     def gen(self, depends_on=None):
         job_id = depends_on
@@ -149,6 +178,7 @@ class Sequential(JobNode):
     def __json__(self):
         return {
             "type": "sequential",
+            "name": self.name,
             "jobs": [
                 job.__json__() for job in self.jobs
             ]
@@ -156,12 +186,16 @@ class Sequential(JobNode):
 
 
 class Parallel(JobNode):
-    def __init__(self, *jobs):
+    def __init__(self, *jobs, name='P'):
+        self.name = name
         self.jobs = jobs
-    
+
+    def output_dir(self, root):
+        return os.path.join(root, self.name)
+
     def gen(self, depends_on=None):
         job_ids = []
-        for job in self.jobs: 
+        for job in self.jobs:
             job_ids.append(job.gen(depends_on=depends_on))
 
         return ':'.join(job_ids)
@@ -169,6 +203,7 @@ class Parallel(JobNode):
     def __json__(self):
         return {
             "type": "parallel",
+            "name": self.name,
             "jobs": [
                 job.__json__() for job in self.jobs
             ]
@@ -191,7 +226,7 @@ standard_run = Sequential(
         Job("v100", "run"),
     )
 )
-    
+
 
 
 def job_acc_cache_status(filename: str):
@@ -204,7 +239,7 @@ def job_acc_cache_status(filename: str):
             # Cache is good
             if "COMPLETED" in job_states:
                 return True
-            
+
             return False
         except json.decoder.JSONDecodeError:
             return False
@@ -235,7 +270,7 @@ def local_cache(cache_key, arg_key="job_id", check_validation=None):
                     return send_file(cache_file, mimetype="application/json")
 
                 # No cached data
-                return result 
+                return result
 
             try:
                 os.makedirs(cache_dir, exist_ok=True)
@@ -270,7 +305,7 @@ def generate_unique_job_name(job_data=None, from_job_id=None):
 
 RSYNC_OK = 0
 RSYNC_TIMEOUT = 2
-RSYNC_ERROR = 1 
+RSYNC_ERROR = 1
 
 def rsync_jobrunner_folder(timeout=5):
     rsync_cmd = f"rsync -az mila:{JOBRUNNER_WORKDIR}/ {JOBRUNNER_LOCAL_CACHE}"
@@ -284,7 +319,7 @@ def rsync_jobrunner_folder(timeout=5):
                     timeout=timeout,
                     shell=True
                 )
-            
+
         print(f"{rsync_cmd} {chrono.timing.value.avg} s")
 
         return 0
@@ -823,7 +858,7 @@ def slurm_integration(app):
             # Try to get job logs using scontrol
             result = remote_command(SLURM_HOST, f'scontrol show job --json {job_id}')
             # result = remote_command(SLURM_HOST, f'sacct --json -j {job_id}')
-            
+
             if not result['success']:
                 return jsonify({'error': f'Failed to get job info: {result["stderr"]}'}), 500
 
