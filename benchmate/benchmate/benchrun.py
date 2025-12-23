28400d0bd45bfa6d8c9131827516a317ac3ed1ac
Setepenre <pierre.delaunay@mila.quebec>
2025-12-23 04:10:42 +0000

    Fix backward compatibility problem with pytorch 2.8 & 2.9

--- benchmate/benchmate/benchrun.py
+++ benchmate/benchmate/benchrun.py
@@ -2,7 +2,9 @@
 
 import os
 import subprocess
+from packaging import version
 
+import torch
 import torch.distributed.run as distrun
 import torch.distributed.elastic.multiprocessing.api as elastic
 import torch.distributed.elastic.multiprocessing.subprocess_handler as sub
@@ -25,24 +27,8 @@ class NewSubprocessHandler(sub.SubprocessHandler):
             **kwargs,
         )
 
-def get_subprocess_handler(
-    entrypoint: str,
-    args,
-    env,
-    stdout: str,
-    stderr: str,
-    local_rank_id: int,
-    numa_options = None
-):
-    return NewSubprocessHandler(
-        entrypoint=entrypoint,
-        args=args,
-        env=env,
-        stdout=stdout,
-        stderr=stderr,
-        local_rank_id=local_rank_id,
-        numa_options=numa_options,
-    )
+def get_subprocess_handler(*args, **kwargs):
+    return NewSubprocessHandler(*args, **kwargs)
 
 
 @contextmanager
